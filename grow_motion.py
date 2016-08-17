60aa45ec2df49ca412ddab5a16fe7b01d9c726da
sinanm89 <sinanm89@gmail.com>
2016-08-17 13:32:03 -0400

    prettified the code a bit

--- grow_motion.py
+++ grow_motion.py
@@ -1,41 +1,32 @@
-# Simple example of reading the MCP3008 analog input channels and printing
-# them all out.
-# Author: Tony DiCola
-# License: Public Domain
+"""
+Garden health measurement system.
+
+Reads humidity, temperature around a plant and in the soil, writes to db and
+takes the picture of the plant.
+"""
 import time
 import os
 from datetime import datetime
 # Import SPI library (for hardware SPI) and MCP3008 library.
-import Adafruit_GPIO.SPI as SPI
+# import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
 
 import picamera
 import dht11
 import RPi.GPIO as GPIO
 
+# for dht11 ambient setup
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 
 DHT11_PIN = 26
 
-def take_ambient_values():
-    
-    instance = dht11.DHT11(pin = DHT11_PIN)
-    result = instance.read()
-
-    if result.is_valid():
-        print("Temperature: %d C" % result.temperature)
-        print("Humidity: %d %%" % result.humidity)
-    else:
-        print("Error: %d" % result.error_code)
-         
-
 # Software SPI configuration #pin:
-CLK  = 18
+CLK = 18
 MISO = 23
 MOSI = 24
-CS   = 25
+CS = 25
 mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
 
 # Hardware SPI configuration:
@@ -57,35 +48,60 @@ delta_wet = dry - wet
 
 average_frequency = 3
 
-while True:
-	avg_humid = 0
-	for i in range(average_frequency):
-	    # Read all the ADC channel values in a list
-	        # The read_adc function will get the value of the specified channel (0-7).
-	    temp_humid = mcp.read_adc(0)
-	    avg_humid += temp_humid
-	    time.sleep(0.5)
-	    # (960 + 962 + 961) / 3 
-	avg_humid = avg_humid / average_frequency
-	# 961 - (600)
-	avg_humid = avg_humid - normalize_wet
-	# 361 / 423 = 0.8, so its pretty moist.
-	avg_humid_percentage = float(avg_humid) / delta_wet
-	time.sleep(0.5)
-        try:
-            camera = picamera.PiCamera()
-        except picamera.exc.PiCameraError:
-            print 'camera borked, moving on.'
-            continue
-        except Exception, e:
-            print 'WOW FATAL EXCEPTION'
-            print e
-            continue
-        now = datetime.now().strftime('%s')
-	pic_name = '{time}_0{humidity}.jpg'.format(time=now, humidity=int(avg_humid_percentage*100))
-	camera.capture('{}/{}'.format(DIR_NAME, pic_name))
-	print 'took {}'.format(pic_name)
-        take_ambient_values()
-	time.sleep(2*60)
 
-        
+def take_ambient_values():
+    """
+    Read the dht11 module.
+
+    The custom dht11 module is on git@github.com:sinanm89/DHT11_Python.git
+    """
+    instance = dht11.DHT11(pin=DHT11_PIN)
+    result = instance.read()
+    if result.is_valid():
+        print("Temperature: %d C" % result.temperature)
+        print("Humidity: %d %%" % result.humidity)
+    else:
+        print("Error: %d" % result.error_code)
+
+
+def take_soil_values():
+    """
+    Read the moisture sensor connected to the MCP3008.
+
+    The important part is mcp.read_adc(channel_number), you can read up to 8
+    which is the number of inputs the physical chip has.
+    """
+    avg_humid = 0
+    for i in range(average_frequency):
+        # Read all the ADC channel values in a list
+        # The read_adc function will get the value of the channel (0-7)
+        temp_humid = mcp.read_adc(0)
+        avg_humid += temp_humid
+        time.sleep(0.5)
+    # (960 + 962 + 961) / 3
+    avg_humid = avg_humid / average_frequency
+    # 961 - (600)
+    avg_humid = avg_humid - normalize_wet
+    # 361 / 423 = 0.8, so its pretty moist.
+    avg_humid_percentage = float(avg_humid) / delta_wet
+    time.sleep(0.5)
+    try:
+        camera = picamera.PiCamera()
+    except picamera.exc.PiCameraError:
+        print 'camera borked, moving on.'
+        continue
+    except Exception, e:
+        print 'WOW FATAL EXCEPTION'
+        print e
+        continue
+    now = datetime.now().strftime('%s')
+    pic_name = '{time}_0{humidity}.jpg'.format(
+        time=now, humidity=int(avg_humid_percentage * 100))
+    camera.capture('{}/{}'.format(DIR_NAME, pic_name))
+    print 'took {}'.format(pic_name)
+
+# main
+while True:
+    take_soil_values()
+    take_ambient_values()
+    time.sleep(2 * 60)
