1ac7eb3289cf9157b1b2a5a969b5b007de801ed6
sinanm89 <sinanm89@gmail.com>
2016-08-22 03:33:02 -0400

    added stuff

---
+++ generic_model.py
@@ -0,0 +1,5 @@
+"""Declare the base here."""
+from sqlalchemy.ext.declarative import declarative_base
+# metadata = MetaData()
+# Base = declarative_base(metadata=metadata)
+Base = declarative_base()
