64487c139549dee9752fbadfc10a8f3dcefc5b61
sinanm89 <sinanm89@gmail.com>
2016-08-25 16:22:05 +0000

    idontrememberwhatIdid.com

--- img_take.py
+++ img_take.py
@@ -3,16 +3,16 @@ import picamera
 
 with picamera.PiCamera(resolution=(1920, 1080)) as camera:
     # Set ISO to the desired value
-    # camera.iso = 100
+    camera.iso = 100
     # Wait for the automatic gain control to settle
     # camera.meter_mode = 'backlit'
     # Now fix the values
     # camera.shutter_speed = camera.exposure_speed
     # camera.exposure_mode = 'spotlight'
     # g = camera.awb_gains
-    camera.awb_mode = 'auto'
+    camera.awb_mode = 'fluorescent'
     camera.vflip = True
-    sleep(1)
+    sleep(2)
     # camera.awb_gains = g
     # Finally, take several photos with the fixed settings
     camera.capture('harambe.jpg')
