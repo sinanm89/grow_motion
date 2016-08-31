63cb61f8a177a6cebb25414f493d87de24326125
sinanm89 <sinanm89@gmail.com>
2016-08-31 05:16:41 +0000

    img take fix

--- img_take.py
+++ img_take.py
@@ -1,18 +1,12 @@
 from time import sleep
+from datetime import datetime
 import picamera
 
 with picamera.PiCamera(resolution=(1920, 1080)) as camera:
-    # Set ISO to the desired value
-    camera.iso = 100
-    # Wait for the automatic gain control to settle
-    # camera.meter_mode = 'backlit'
-    # Now fix the values
-    # camera.shutter_speed = camera.exposure_speed
-    # camera.exposure_mode = 'spotlight'
-    # g = camera.awb_gains
-    camera.awb_mode = 'fluorescent'
     camera.vflip = True
-    sleep(2)
-    # camera.awb_gains = g
-    # Finally, take several photos with the fixed settings
-    camera.capture('harambe.jpg')
+    camera.awb_mode = 'sunlight'
+    camera.contrast = 30
+    camera.sharpness = 100
+    new_file ='one_shot_{}.jpg'.format(datetime.now().strftime('%s'))
+    sleep(1)
+    camera.capture(new_file)
